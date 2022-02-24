8665a4d87b28dc0e4375e4cc80b8b018500aa7af
Alex Reed <alex.reed@gatsbyjs.com>
2022-02-24 15:52:52 -0500

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-datocms-homepage.git)

---
+++ src/components/header.js
@@ -0,0 +1,170 @@
+import * as React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import { Menu, X } from "react-feather"
+import {
+  Container,
+  Flex,
+  FlexList,
+  Space,
+  NavLink,
+  Button,
+  InteractiveIcon,
+  Nudge,
+  VisuallyHidden,
+} from "./ui"
+import {
+  mobileNavOverlay,
+  mobileNavLink,
+  desktopHeaderNavWrapper,
+  mobileHeaderNavWrapper,
+  mobileNavSVGColorWrapper,
+} from "./header.css.ts"
+import NavItemGroup from "./nav-item-group"
+import BrandLogo from "./brand-logo"
+
+export default function Header() {
+  const data = useStaticQuery(graphql`
+    query {
+      layout {
+        header {
+          id
+          navItems {
+            ... on NavItem {
+              id
+              href
+              text
+            }
+            ... on NavItemGroup {
+              id
+              name
+              navItems {
+                id
+                href
+                text
+                description
+                icon {
+                  alt
+                  gatsbyImageData
+                }
+              }
+            }
+          }
+          cta {
+            id
+            href
+            text
+          }
+        }
+      }
+    }
+  `)
+
+  const { navItems, cta } = data.layout.header
+
+  const [isOpen, setOpen] = React.useState(false)
+
+  const isLinkGroup = React.useCallback((link) => {
+    return "navItems" in link
+  }, [])
+
+  React.useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflowY = "hidden"
+    } else {
+      document.body.style.overflowY = "visible"
+    }
+  }, [isOpen])
+
+  return (
+    <header>
+      <Container className={desktopHeaderNavWrapper}>
+        {/* Desktop / Tablet - Header / Nav */}
+        <Space size={2} />
+        <Flex variant="spaceBetween">
+          <NavLink to="/">
+            <VisuallyHidden>Home</VisuallyHidden>
+            <BrandLogo />
+          </NavLink>
+          <nav>
+            <FlexList gap={4}>
+              {navItems &&
+                navItems.map((navItem) => (
+                  <li key={navItem.id}>
+                    {!isLinkGroup(navItem) ? (
+                      <NavLink to={navItem.href}>{navItem.text}</NavLink>
+                    ) : (
+                      <NavItemGroup
+                        name={navItem.name}
+                        navItems={navItem.navItems}
+                      />
+                    )}
+                  </li>
+                ))}
+            </FlexList>
+          </nav>
+          <div>{cta && <Button to={cta.href}>{cta.text}</Button>}</div>
+        </Flex>
+      </Container>
+      {/* Mobile - Header / Nav */}
+      <Container className={mobileHeaderNavWrapper[isOpen ? "open" : "closed"]}>
+        <Space size={2} />
+        <Flex variant="spaceBetween">
+          <span
+            className={
+              mobileNavSVGColorWrapper[isOpen ? "reversed" : "primary"]
+            }
+          >
+            <NavLink to="/">
+              <VisuallyHidden>Home</VisuallyHidden>
+              <BrandLogo />
+            </NavLink>
+          </span>
+          <Flex>
+            <Space />
+            <div>
+              {cta && (
+                <Button to={cta.href} variant={isOpen ? "reversed" : "primary"}>
+                  {cta.text}
+                </Button>
+              )}
+            </div>
+            <Nudge right={3}>
+              <InteractiveIcon
+                title="Toggle menu"
+                onClick={() => setOpen(!isOpen)}
+                className={
+                  mobileNavSVGColorWrapper[isOpen ? "reversed" : "primary"]
+                }
+              >
+                {isOpen ? <X /> : <Menu />}
+              </InteractiveIcon>
+            </Nudge>
+          </Flex>
+        </Flex>
+      </Container>
+      {isOpen && (
+        <div className={mobileNavOverlay}>
+          <nav>
+            <FlexList responsive variant="stretch">
+              {navItems &&
+                navItems.map((navItem) => (
+                  <li key={navItem.id}>
+                    {!isLinkGroup(navItem) ? (
+                      <NavLink to={navItem.href} className={mobileNavLink}>
+                        {navItem.text}
+                      </NavLink>
+                    ) : (
+                      <NavItemGroup
+                        name={navItem.name}
+                        navItems={navItem.navItems}
+                      />
+                    )}
+                  </li>
+                ))}
+            </FlexList>
+          </nav>
+        </div>
+      )}
+    </header>
+  )
+}
